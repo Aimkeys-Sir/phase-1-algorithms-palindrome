2ea57b44a77d6090b30ab5fe1778b9b0c623e407
Aimkeys Kelvin Mwaura <kelvin.ngugi@student.moringaschool.com>
2022-06-19 08:53:50 +0300

    Challenge complete

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 function isPalindrome(word) {
   // Write your algorithm here
-}
+  const splitted=word.split("")
+  let unshifted=[];
+  splitted.forEach(char=>{
+    unshifted.unshift(char)
+  })
+  return unshifted.join("")===word?true:false
 
+}
+//console.log(isPalindrome("madam"))
 /* 
-  Add your pseudocode here
+  Add your pseudocode 
+  split then unshift
+  then join the unshifted
 */
 
 /*
   Add written explanation of your solution here
 */
 
-// You can run `node index.js` to view these console logs
+//You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
   console.log("Expecting: true");
@@ -23,3 +32,20 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+//Your code here
+class dog
+{
+    name;breed;
+    constructor(name,breed)
+    {
+        this.name=name
+        this.breed=breed
+    }
+
+}
+//OOP ---inheritance
+const aDog=new dog("German Shepherd","Poodle")
+console.log(`A_Dog${aDog}`)
+
+//JSON{key:value}
+console.log(JSON.stringify(aDog))
